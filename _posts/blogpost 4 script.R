fcdf8356ac0713837f52b0c454af9e6397dfad78
dberger2 <dberger2@ncsu.edu>
2021-10-31 14:09:58 -0400

    Pushing blogpost for project 2

--- _posts/blogpost 4 script.R
+++ _posts/blogpost 4 script.R
@@ -1,5 +1,5 @@
-render(
-  "_posts/2021-10-5-ST558-Blogpost3.Rmd",
+rmarkdown::render(
+  "_posts/2021-10-31-ST558-Blogpost4.Rmd",
   output_format = "github_document" (html_preview = FALSE),
   output_dir = "_posts",
     encoding = "UTF-8"
